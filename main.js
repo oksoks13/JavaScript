078995f64f3fb5b349d6a75945260bb39e5c5653
Oksana <oksanasurotiyk@gmail.com>
2020-02-26 07:26:25 +0200

    JS

---
+++ main.js
@@ -0,0 +1,28 @@
+/*task1
+var family = prompt('Do you have sister?', '');
+
+if (family == 'yes') {
+    alert('its great');
+}
+else {
+    alert('Also not bad')
+*/
+/*task2
+var i = 1;
+while (i < 3) {
+    console.log('Value of i: ' + i);
+    i++;
+}*/
+/*task3
+var a = +prompt('3');
+switch (a) {
+    case 0:
+        console.log(false);
+        break;
+    case 1:
+        console.log(false);
+        break;
+    case 3:
+        console.log(true);
+        break;
+}*/
